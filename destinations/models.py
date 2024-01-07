784d0e5e86ad5e225088d83c28490f7df250e733
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-07 16:16:03 +0600

    restrict unique destination name

--- destinations/models.py
+++ destinations/models.py
@@ -6,5 +6,10 @@ class Destination(models.Model):
     active = models.BooleanField(default=True)
     name = models.CharField(max_length=100, null=False, unique=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'], name='unique_destination_name')
+        ]
+
     def __str__(self):
         return self.name
