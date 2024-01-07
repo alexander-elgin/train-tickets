498c26863fc52561f0dd2356dced953d796a6480
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-07 17:14:59 +0600

    create carriage indexes

--- carriages/models.py
+++ carriages/models.py
@@ -4,10 +4,10 @@ from trains.models import Train
 
 
 class Carriage(models.Model):
-    id = models.AutoField(primary_key=True)
-    train = models.ForeignKey(Train, on_delete=models.CASCADE, default=None)
-    sleeping = models.BooleanField(default=False)
-    active = models.BooleanField(default=True)
+    id = models.AutoField(primary_key=True, db_index=True)
+    train = models.ForeignKey(Train, on_delete=models.CASCADE, default=None, db_index=True)
+    sleeping = models.BooleanField(default=False, db_index=True)
+    active = models.BooleanField(default=True, db_index=True)
 
     def __str__(self):
         return self.id
