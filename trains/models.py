12b96a2aa0bb892375f4a117f87c76aa9e29777b
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-07 17:13:32 +0600

    create train indexes

--- trains/models.py
+++ trains/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Train(models.Model):
-    id = models.AutoField(primary_key=True)
-    active = models.BooleanField(default=True)
+    id = models.AutoField(primary_key=True, db_index=True)
+    active = models.BooleanField(default=True, db_index=True)
 
     def __str__(self):
         return self.id
