432b24648a8d8999c8931a780d538dea0334e910
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-07 17:12:08 +0600

    create destination indexes

--- destinations/models.py
+++ destinations/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Destination(models.Model):
-    id = models.AutoField(primary_key=True)
-    active = models.BooleanField(default=True)
-    name = models.CharField(max_length=100, null=False, unique=True)
+    id = models.AutoField(primary_key=True, db_index=True)
+    active = models.BooleanField(default=True, db_index=True)
+    name = models.CharField(max_length=100, null=False, unique=True, db_index=True)
 
     class Meta:
         constraints = [
