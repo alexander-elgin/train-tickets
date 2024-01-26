1a6d1da6aeab4f7191ecc64c9e0c1f57d0f04fb9
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-26 17:29:26 +0600

    introduce field validation

--- destinations/serializers.py
+++ destinations/serializers.py
@@ -7,3 +7,9 @@ class DestinationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Destination
         fields = ['id', 'active', 'name']
+
+    def validate_name(self, value):
+        if value is not None and len(value) < 2:
+            raise serializers.ValidationError('The name is too short')
+
+        return value
