1418b2979fc28089c20d69cc815e6f90b673c223
Alexander Elgin <sascha.elgin@gmail.com>
2024-01-26 16:13:26 +0600

    create an endpoint creating destination

--- destinations/views.py
+++ destinations/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,3 +10,12 @@ class DestinationView(APIView):
     def get(self, request):
         serializer = DestinationSerializer(Destination.objects.all(), many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DestinationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
